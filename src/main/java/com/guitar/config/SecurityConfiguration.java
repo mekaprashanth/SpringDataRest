41e47a225d2ca328514062af2262ec0a2e2af596
Sanjay Paudel <sanjaypaudel007@gmail.com>
2016-06-03 15:27:24 -0500

    Implement Constraint Validation through use of Controller Advice

--- src/main/java/com/guitar/config/SecurityConfiguration.java
+++ src/main/java/com/guitar/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,6 +18,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.password("admin").roles("ADMIN").and().withUser("moderator").password("moderator")
 				.roles("ADMIN", "USER");
 		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.csrf().disable();
 	}
 
 }
